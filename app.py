b9cf86280b5b2a85a2842b122e19966216a0d768
woonhk90 <wonho7344@gmail.com>
2022-06-28 11:30:15 +0900

    app, index wonho update

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from pymongo import MongoClient
 import certifi
 from dotenv import load_dotenv
 import os
+from datetime import datetime
 load_dotenv()
 app = Flask(__name__)
 
@@ -28,6 +29,64 @@ def home():
 
 # TODOLIST ------------------------------------------------------------------- #
 
+#=============================================================================== todo-list 글 작성(엔터 눌렸을 때)
+@app.route("/todoWriteAction", methods=["POST"])
+def todoWriteAction():
+    todo_receive = request.form['togoVal']
+    name_receive = request.form['nameVal']
+    total = len(list(db.todo.find({})))+1
+
+    now = datetime.now()
+    doc = {
+        'todo': todo_receive,
+        'name': name_receive,
+        'done':0,
+        'num':total,
+        'dateTime':now
+    }
+    db.todo.insert_one(doc)
+    return jsonify({'msg': '완료!'})
+
+#========================================================================================== todo-list 글 가져옴
+@app.route("/getTodoList", methods=["GET"])
+def getTodoList():
+    nameVal = request.args.get('nameVal')
+    print('nameVal->',nameVal)
+    test_info = list(db.todo.find({'name':nameVal}))
+    i=0
+    for info in test_info:
+        i=i+1
+        db.todo.update_one({'_id':info['_id'],'name':nameVal},{'$set':{'num':i}})
+
+    all_todo = list(db.todo.find({'name':nameVal},{'_id':False}))
+    print(all_todo)
+    return jsonify({'msg': all_todo})
+
+#========================================================================================== todo-list 글 수정
+@app.route("/todoModify", methods=["POST"])
+def todoModify():
+    data_receive = request.form['modiData']
+    num_receive = int(request.form['modiNum'])
+    doc = {
+        'todo':data_receive,
+        'num':num_receive
+    }
+    print(data_receive, num_receive)
+    db.todo.update_one({'num':num_receive},{'$set':{'todo':data_receive}})
+    return jsonify({'msg': '완료!'})
+
+#=========================================================================================== todo-list 글 지움
+@app.route("/deleteAction", methods=["POST"])
+def deleteAction():
+    num_receive = int(request.form['todoNum'])
+    name_receive = request.form['nameVal']
+    doc = {
+        'num':num_receive,
+        'name':name_receive
+    }
+    print(doc)
+    db.todo.delete_one(doc)
+    return jsonify({'msg': '완료!'})
 
 # QUOTE ---------------------------------------------------------------------- #
 
